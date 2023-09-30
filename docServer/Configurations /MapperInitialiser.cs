20e99dbf42c9616198224bfe88556fc76ee36c6c
Armah <77860336+armah001@users.noreply.github.com>
2023-09-30 14:18:16 +0000

    implemented configurations

---
+++ docServer/Configurations /MapperInitialiser.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using AutoMapper;
+using docServer.Models;
+using docServer.Data;
+using docServer.DTOs;
+
+namespace docServer.Configurations
+{
+	public class MapperInitialiser : Profile
+    {
+		public MapperInitialiser()
+		{
+            CreateMap<Document, DocumentDTO>().ReverseMap();
+
+            CreateMap<Document, CreateDocumentDTO>().ReverseMap();
+
+            CreateMap<Document, UpdateDocumentDTO>().ReverseMap();
+
+            CreateMap<User, UserDTO>().ReverseMap();
+
+            //CreateMap<ApiUser, LoginUserDTO>().ReverseMap();
+        }
+    }
+}
+
